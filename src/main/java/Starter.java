b455875b176e50f2e622df7c3130ee1c0ba1b934
SadeepaNHerath <sadeepahearth@gmail.com>
2025-04-26 10:52:48 +0530

    Add initial implementation of Tic-Tac-Toe application with main classes and FXML views

---
+++ src/main/java/Starter.java
@@ -0,0 +1,18 @@
+package edu.iCET;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/home_window.fxml"))));
+        stage.show();
+    }
+}
